2b3d984a0bed5ba98dc66a8c235d6389936fb977
weerobots <143899357+weerobots@users.noreply.github.com>
2024-04-11 14:19:40 -0500

    clanup imports

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -7,11 +7,9 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkBase.ExternalFollower;
 
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -19,10 +17,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.util.CANSparkMaxUtil;
 import frc.lib.util.CANSparkMaxUtil.Usage;
 import frc.robot.Constants;
-import frc.robot.Pref;
 import frc.robot.Constants.CANIDConstants;
-import monologue.Logged;
+import frc.robot.Pref;
 import monologue.Annotations.Log;
+import monologue.Logged;
 
 
 public class ClimberSubsystem extends SubsystemBase implements Logged{
